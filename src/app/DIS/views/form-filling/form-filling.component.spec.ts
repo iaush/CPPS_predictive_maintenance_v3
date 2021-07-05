ae444163ad75e796d73dea47c8485e2de53974cd
joohong89 <joohong89@gmail.com>
2021-07-05 18:07:06 +0800

    Updated form fill test to not fail

--- src/app/DIS/views/form-filling/form-filling.component.spec.ts
+++ src/app/DIS/views/form-filling/form-filling.component.spec.ts
@@ -1,6 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FormFillingComponent } from './form-filling.component';
+import {ToastService} from '@dis/services/message/toast.service';
+import {NotificationService} from '@progress/kendo-angular-notification';
+import {CustomDialogService} from '@dis/services/message/custom-dialog.service';
+import {DialogContainerService, DialogService} from '@progress/kendo-angular-dialog';
+import {HttpClientModule} from '@angular/common/http';
+import {TranslateModule} from '@ngx-translate/core';
 
 describe('FormFillingComponent', () => {
   let component: FormFillingComponent;
@@ -8,7 +14,9 @@ describe('FormFillingComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FormFillingComponent ]
+      declarations: [ FormFillingComponent ],
+      providers: [ToastService, NotificationService, CustomDialogService, DialogService, DialogContainerService],
+      imports: [HttpClientModule, TranslateModule.forRoot()]
     })
     .compileComponents();
   });
