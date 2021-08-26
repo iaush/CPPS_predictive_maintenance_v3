9a2ba937bd391fc9131ce901dd1b4753760bb917
Michelle Ang <Michelle_Ang@simtech.a-star.edu.sg>
2021-08-26 15:13:20 +0800

    chart hover when routerlink active, thanks JH.

--- src/app/DIS/views/dashboard-one/dashboard-one.component.ts
+++ src/app/DIS/views/dashboard-one/dashboard-one.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import {MocksLocalService} from '@dis/services/mocks/mocks.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-dashboard-one',
@@ -7,10 +9,15 @@ import {MocksLocalService} from '@dis/services/mocks/mocks.service';
   styleUrls: ['./dashboard-one.component.scss']
 })
 export class DashboardOneComponent implements OnInit {
-
   data: any = [];
 
-  constructor(private mocksLocalService: MocksLocalService) { }
+  constructor(private mocksLocalService: MocksLocalService , private router: Router) { 
+  }; 
+
+  Navigateto= function (url) {
+    this.router.navigateByUrl(url);
+};
+
 
   ngOnInit(): void {
     this.data = [...this.mocksLocalService.getSampleDataForGrid(), ...this.mocksLocalService.getSampleDataForGrid()];
