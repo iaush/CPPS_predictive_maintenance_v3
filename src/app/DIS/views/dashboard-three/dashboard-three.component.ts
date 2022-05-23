2d2b571843894bb05ee5fae5b34718a1a8b9b4e6
joohong89 <joohong89@gmail.com>
2022-05-23 09:35:16 +0800

    Added range to example

--- src/app/DIS/views/dashboard-three/dashboard-three.component.ts
+++ src/app/DIS/views/dashboard-three/dashboard-three.component.ts
@@ -3,6 +3,7 @@ import {DataBindingDirective} from '@progress/kendo-angular-grid';
 import {employees} from '@dis/services/mocks/sampleDataForGrid';
 import { process } from '@progress/kendo-data-query';
 
+
 @Component({
   selector: 'app-dashboard-three',
   templateUrl: './dashboard-three.component.html',
@@ -13,33 +14,22 @@ export class DashboardThreeComponent implements OnInit {
 
   constructor() { }
 
-
-  data: any[] = [
-    {
-      kind: 'Hydroelectric',
-      share: 0.175,
-    },
-    {
-      kind: 'Nuclear',
-      share: 0.238,
-    },
-    {
-      kind: 'Coal',
-      share: 0.118,
-    },
-    {
-      kind: 'Solar',
-      share: 0.052,
-    },
-    {
-      kind: 'Wind',
-      share: 0.225,
-    },
-    {
-      kind: 'Other',
-      share: 0.192,
-    },
+  public weatherData = [
+    { month: 'January', min: 0.5, max: 0.65 },
+    { month: 'February', min:  0.5, max: 0.65 },
+    { month: 'March', min:  0.5, max: 0.65 },
+    { month: 'April', min:  0.5, max: 0.65 },
+    { month: 'May', min:  0.5, max: 0.65 },
+    { month: 'June', min:  0.6, max: 0.75 },
+    { month: 'July', min:  0.7, max: 0.85 },
+    { month: 'August', min:  0.5, max: 0.65 },
+    { month: 'September', min:  0.5, max: 0.65 },
+    { month: 'October', min:  0.5, max: 0.65 },
+    { month: 'November', min:  0.5, max: 0.65 },
+    { month: 'December', min:  0.5, max: 0.65 }
   ];
+
+
 
   @ViewChild(DataBindingDirective) dataBinding: DataBindingDirective;
   public gridData: any[] = employees;
@@ -109,4 +99,11 @@ export class DashboardThreeComponent implements OnInit {
     this.gridView = this.gridData;
   }
 
+  public labelContentFrom(e): string {
+    return `${ e.value.from }`;
+  }
+
+  public labelContentTo(e): string {
+    return `${ e.value.to } `;
+  }
 }
