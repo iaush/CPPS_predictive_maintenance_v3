77bb7c1d83a7b87f294d39209b8ea5af9e835388
joohong89 <joohong89@gmail.com>
2022-05-23 11:04:25 +0800

    added constant file to store chart font-settings

---
+++ src/app/DIS/settings/chart.config.ts
@@ -0,0 +1,9 @@
+const chartFontFamily = 'sans-serif';
+
+export const chartConfig = {
+  mainTitleFont: `1.3vw ${chartFontFamily}`,
+  axisTitleFont: `0.75vw ${chartFontFamily}`,
+  axisLabelFont: `0.5vw ${chartFontFamily}`,
+  legendFont:  `0.5vw ${chartFontFamily}`,
+
+};
