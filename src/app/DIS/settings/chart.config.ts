e1f604a45c1b3be49f4895e8d24be326215edae9
joohong89 <joohong89@gmail.com>
2022-05-30 14:16:21 +0800

    re-adjust design to be viewable on TEAMS

--- src/app/DIS/settings/chart.config.ts
+++ src/app/DIS/settings/chart.config.ts
@@ -2,9 +2,9 @@ const chartFontFamily = 'sans-serif';
 
 export const chartConfig = {
   mainTitleFont: `1.3vw ${chartFontFamily}`,
-  axisTitleFont: `0.75vw ${chartFontFamily}`,
-  axisLabelFont: `0.5vw ${chartFontFamily}`,
-  legendFont:  `0.5vw ${chartFontFamily}`,
+  axisTitleFont: `1vw ${chartFontFamily}`,
+  axisLabelFont: `1vw ${chartFontFamily}`,
+  legendFont:  `1vw ${chartFontFamily}`,
   markerSize:  15,
   lineWidth: 3,
   majorLineWidth: 1,
