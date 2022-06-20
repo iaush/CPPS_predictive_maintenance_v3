ea3f01add5ce4e1784104fe8b5c6c1690e69162b
Tan Joo Hong <tan_joo_hong@simtech.a-star.edu.sg>
2022-06-20 14:43:44 +0800

    updated samples and chart settings

--- src/app/DIS/settings/chart.config.ts
+++ src/app/DIS/settings/chart.config.ts
@@ -10,7 +10,7 @@ const legendType: LegendMarkers = {
 export const chartConfig = {
   mainTitleFont: `1.3vw ${chartFontFamily}`,
   axisTitleFont: `1vw ${chartFontFamily}`,
-  axisLabelFont: `1vw ${chartFontFamily}`,
+  axisLabelFont: `0.75vw ${chartFontFamily}`,
   legendFont:  `1vw ${chartFontFamily}`,
   markerSize:  15,
   lineWidth: 3,
