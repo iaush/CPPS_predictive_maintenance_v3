932ebe318401aae57303efcc5f7a5d2c156ef42a
TR-PMS-Admin <tr-pms-admin@gmail.com>
2022-07-14 13:25:09 +0800

    edited content

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,36 @@
 import { addonEnvironment } from '@dis/settings/environments/environment.prod';
 
+const KEYCLOAK_URL = 'https://172.20.105.242/';
+
 export const environment = {
-  production: true,
+
+  production: false,
+  DEV_TEST_USER: {
+    id: 'Dev User 1',
+    username: 'devuser1',
+    email: 'devuser1@test.com',
+    firstName: 'dev',
+    lastName: 'user',
+    enabled: true,
+    emailVerified: true,
+    totp: true
+  },
+  KEYCLOAK_URL: KEYCLOAK_URL + 'auth',
+  KEYCLOAK_REALM: 'demo1',
+  KEYCLOAK_CLIENT: 'DEMO_CLIENT', // Please add the client name(Id), 'INEVNTORY APP' is just a demo client name
+  API_ROOT: '',                     // Please add your API Root
+  APP_ROOT: 'http://localhost:4200',                     // Please add your APP Root
+  KEYCLOAK_GET_CLIENT_ROLES_1: KEYCLOAK_URL + 'auth/admin/realms/demo1/users/',
+  KEYCLOAK_GET_CLIENT_ROLES_2: '/role-mappings/clients/',
+  KEYCLOAK_GET_CLIENT_ROLES_3: '/composite',
   ...addonEnvironment
 };
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
