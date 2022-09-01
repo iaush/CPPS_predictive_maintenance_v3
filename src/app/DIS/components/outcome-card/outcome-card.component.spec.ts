0403effd8dfacb71154c8c5a1a72346d2bcced86
Tan Joo Hong <tan_joo_hong@simtech.a-star.edu.sg>
2022-09-01 12:31:06 +0800

    Adde overview stepper and outcome card component

---
+++ src/app/DIS/components/outcome-card/outcome-card.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { OutcomeCardComponent } from './outcome-card.component';
+
+describe('OutcomeCardComponent', () => {
+  let component: OutcomeCardComponent;
+  let fixture: ComponentFixture<OutcomeCardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ OutcomeCardComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(OutcomeCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
